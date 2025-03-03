804b66f657869af489763b72ba17bbfac6253639
GosiKrisztian <gosikrisz@gmail.com>
2025-03-03 14:54:00 +0100

    kód_lefut_01

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            # 'control_vehicle = gos_aec_turtlesim.control_vehicle:main',
+            'rajz = gos_aec_turtlesim.rajz:main',
         ],
     },
 )
